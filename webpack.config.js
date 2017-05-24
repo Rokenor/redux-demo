2242414c5ea87e2d22411a404c392caaa793fe28
Rokenor <rokenor@zeoalliance.com>
2017-05-24 17:28:40 +0300

    Tutorial done Ready for additional coding

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const NpmInstallPlugin = require('npm-install-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const precss = require('precss');
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
@@ -17,7 +19,6 @@ module.exports = {
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin(),
         new NpmInstallPlugin()
     ],
     module: {
@@ -26,19 +27,26 @@ module.exports = {
                 test: /\.js$/,
                 loaders: ['eslint'],
                 include: [
-                    path.resolve(__dirname, "src"),
-                ],
+                    path.resolve(__dirname, 'src'),
+                ]
             }
         ],
         loaders: [
             {
-                loaders: ['babel-loader'],
+                loaders: ['react-hot', 'babel-loader'],
                 include: [
-                    path.resolve(__dirname, "src"),
+                    path.resolve(__dirname, 'src'),
                 ],
                 test: /\.js$/,
                 plugins: ['transform-runtime'],
+            },
+            {
+                test:   /\.css$/,
+                loader: 'style-loader!css-loader!postcss-loader'
             }
         ]
+    },
+    postcss: function () {
+        return [autoprefixer, precss];
     }
 };
